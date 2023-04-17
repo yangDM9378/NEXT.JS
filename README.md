6a8648139059134819ee7aebcb871705b895b128
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-04-17 23:48:41 +0900

    routing 실습

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ### 정적 라우팅
 - commit#1 - 12버전 routing
 - commit#2 - 13버전 routing
+- commit#3 - routing 실습
--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function ProductsPage() {
   return <div>제품소개</div>;
 }
