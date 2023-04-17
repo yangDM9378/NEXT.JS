b20a9e07ead645ea8c336185f118ff83f5633db4
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-04-18 00:52:49 +0900

    Dynamic Routing 내용 추가

--- my-app/src/app/products/[slug]/page.tsx
+++ my-app/src/app/products/[slug]/page.tsx
@@ -7,3 +7,10 @@ type Props = {
 export default function PantsPage({ params }: Props) {
   return <div>{params.slug} 제품 설명 페이지</div>;
 }
+
+export function genrateStaticParams() {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
