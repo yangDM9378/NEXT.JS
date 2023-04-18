30a8761f96f818834823d1ba0a089113c519e933
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-04-18 22:13:12 +0900

    Link

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 import styles from "./layout.module.css";
 
@@ -17,8 +18,9 @@ export default function RootLayout({
         <header className={styles.header}>
           <h1>Demo my App</h1>
           <nav className={styles.nav}>
-            <a href="">Contact</a>
-            <a href="">About</a>
+            <Link href="/products">Product</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
           </nav>
         </header>
         {children}
