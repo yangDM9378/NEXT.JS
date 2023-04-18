9976814a3b51a30491e58e45bdb51678012abb2b
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-04-18 22:31:51 +0900

    Link 내용추가

--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
+const products = ["shirt", "pant", "skirt"];
+
 export default function ProductsPage() {
-  return <div>제품소개</div>;
+  return (
+    <>
+      <h1>제품소개</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <Link href={`products/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
