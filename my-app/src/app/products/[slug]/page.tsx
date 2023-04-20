c2bba0c1620d13ad88e91bcaa2d9ffbb424bb235
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-04-21 00:37:08 +0900

    SSG로 렌더링

--- my-app/src/app/products/[slug]/page.tsx
+++ my-app/src/app/products/[slug]/page.tsx
@@ -13,19 +13,20 @@ export function generateMetadata({ params: { slug } }: Props) {
   };
 }
 
-export default function PantsPage({ params: { slug } }: Props) {
-  const product = getProduct(slug);
+export default async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
+
   if (!product) {
     notFound();
   }
   // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <h1>{product} 제품 설명 페이지</h1>;
+  return <h1>{product.name} 제품 설명 페이지</h1>;
 }
 
-export function generateStaticParams() {
+export async function generateStaticParams() {
   // 모든 제품의 페이지들을 미리 만들어 두게 함 (SSG)
-  const products = getProducts();
-  return products.map((product) => ({
-    slug: product,
+  const products = await getProducts();
+  return products.map(({ id }) => ({
+    slug: id,
   }));
 }
--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,16 +1,16 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
 
-export default function ProductsPage() {
-  // 서버파일(데이터페이스)에 데이터 가져와 사용하기
-  const products = getProducts();
+export default async function ProductsPage() {
+  // 서버파일(데이터베이스)에 데이터 가져와 사용하기
+  const products = await getProducts();
   return (
     <>
       <h1>제품소개</h1>
       <ul>
-        {products.map((product, index) => (
+        {products.map(({ id, name }, index) => (
           <li key={index}>
-            <Link href={`products/${product}`}>{product}</Link>
+            <Link href={`products/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>
