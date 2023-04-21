381e7f0c7ac13199dae803bf13d795190744aa38
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-04-21 13:53:53 +0900

    v13 fetch 활용 SSG, ISR로 렌더링

--- README.md
+++ README.md
@@ -8,3 +8,16 @@
 - commit#4 - Dynamic Routing
 - commit#5 - Dynamic Routing 내용 추가
 - commit#6 - Not Found
+### 레이아웃
+- commit#7 - layout header
+- commit#8 - layout product header
+- commit#9 - Link
+- commit#10 - Link 내용추가
+### SEO
+- commit#11 - SEO
+### 렌더링
+- commit#12 - v13 서버컴포넌트와 클라이언트컴포넌트
+- commit#13 - v13 서버파일(데이터베이스)에서 데이터 가져오기 리팩토링
+- commit#14 - SSG로 렌더링
+- commit#15 - v13 ISR로 렌더링
+- commit#16 - v13 fetch 활용 SSG, ISR로 렌더링
--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,12 +1,19 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
+import styles from "./page.module.css";
 
 // 3초마다 데이터 받게
-export const revalidate = 10;
+// export const revalidate = 10;
 
 export default async function ProductsPage() {
   // 서버파일(데이터베이스)에 데이터 가져와 사용하기
   const products = await getProducts();
+  const res = await fetch("https://meowfacts.herokuapp.com", {
+    // next: { revalidate: 0 }, //SSR
+    next: { revalidate: 3 }, //SSI
+  });
+  const data = await res.json();
+  const factText = data.data[0];
   return (
     <>
       <h1>제품소개</h1>
@@ -17,6 +24,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
+      <article className="styles.article">{factText}</article>
     </>
   );
 }
